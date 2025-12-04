cb2dc9e11040209769850cbdc2b02e8166cf91de
Madhu Dhanaveni <dhanavenimadhu23@gmail.com>
2025-12-04 12:26:32 +0000

    backend url updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import Nav from './components/Nav'
-import Login from './components/Login'
-import Register from './components/Register'
-import Allprofiles from './components/Allprofiles'
-import { Route, Routes } from 'react-router-dom'
+import React from "react";
+import Nav from "./component/Nav";
+import Login from "./component/Login";
+import Register from "./component/Register";
+import Allprofiles from "./component/Allprofiles";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className='mx-auto flex flex-col'>
-     <Nav />
-     <Routes>
-       <Route path='/' element={<Login />} />
-       <Route path='/register' element={<Register />} />
-       <Route path='/profile' element={<Allprofiles />} />
-     </Routes>
+    <div className="mx-auto flex flex-col">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Allprofiles />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
